a0d0773aca9b5092a1bc6f9fba00688682d7ac89
SoundsSerious <keavin7@ottermatics.com>
2024-04-26 16:17:11 -0400

    update hardware loops

--- waveware/hardware.py
+++ waveware/hardware.py
@@ -41,6 +41,8 @@ from aiobotocore.session import get_session
 from collections import deque
 from expiringdict import ExpiringDict
 
+from imusensor.MPU9250 import MPU9250
+
 import asyncpio
 
 try:
@@ -65,7 +67,7 @@ log = logging.getLogger("data")
 
 #TODO: default values to run system
 LABEL_DEFAULT = {
-    "title": "test"
+    "title": "test",
     "hs_in": 1/1000., #m
     "ts-in": 1.0, #s
     "max-torque": None,
@@ -81,29 +83,25 @@ class hardware_control:
     #hw access
     pi = None
 
-    #encoder output
-    a_ch_z:int = None
-    b_ch_z:int = None
-    hz_1:int = None
-    hz_2:int = None
-    hz_3:int = None
-    hz_4:int = None
-
-    mpu_addr: hex = 0x68#0x69 #3.3v
-    bmp_280: hex = 0x40
-
-    #motor control
-    en_pin: int = None
-    a_ch: int = None
-    b_ch: int = None
-    hlfb_pin: int = None
-    hlfb_mode: int = 'ppr'
-    drive_mode: int = 'pos_input'
-    drive_modes: int = ['pos_input','2pos']
+    #pinout / registers
+    encoder_pins: list = None  #[(A,B),(A2,B2)...]
+    echo_pins: list = None #[1,2,3]
+    #potentiometer_pin: int = None
+    # #i2c addr
+    # mpu_addr: hex = 0x68#0x69 #3.3v
+    # bmp_280_addr: hex = 0x40
+    # #motor control
+    # en_pin: int = None
+    # a_ch: int = None
+    # b_ch: int = None
+    # hlfb_pin: int = None    
+    # hlfb_mode: int = 'ppr'
+    # drive_mode: int = 'pos_input'
+    # drive_modes: int = ['pos_input','2pos']
 
     #config flags
     active = False
-    poll_rate = 1.0 / 20.
+    poll_rate = 1.0 / 100.
     window = 60.0
     
     #Data Storage
@@ -112,7 +110,7 @@ class hardware_control:
     cache: ExpiringDict
 
     #TODO: pins_def
-    def  __init__(self,a_ch_z:int,b_ch_z:int,hz_1:int,hz_2:int,hz_3:int,,hz_4:int,winlen = 1000,*args,**kwargs):
+    def  __init__(self,encoder_ch:list,echo_ch:list,winlen = 1000,*args,**kwargs):
         self.start_time = time.time()
         self.is_fake_init = lambda: True if (time.time() - self.start_time) <  FAKE_INIT_TIME else False
 
@@ -125,22 +123,32 @@ class hardware_control:
         self.cache = ExpiringDict(max_len=self.window * 2 / self.poll_rate, max_age_seconds=self.window * 2)
 
         self.last = {} #TODO: init pins as 0
-        #pin_enc_A
-        #pin_enc_B
+        self.echo_pins = echo_ch
+        self.encoder_pins = encoder_ch
+        
 
+    #Setup & Shutdown
+    def setup(self):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self._setup_hardware())
 
-    async def setup_hardware(self):
+    async def _setup_hardware(self):
         self.pi = asyncpio.pi()
-        await self.pi.connect()
+        con =  await self.pi.connect()
 
         await self.setup_encoder()
         await self.setup_echo_sensors()
         await self.setup_motor_control()
+
         #await self.setup_i2c_sensors()
         #await self.setup_gpio_sensors()
         #await self.setup_adc_sensors()
 
-    async def stop(self):
+    def stop(self):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self._stop())
+
+    async def _stop(self):
         """
         Cancel the rotary encoder decoder.
         """
@@ -150,111 +158,44 @@ class hardware_control:
         await self._cb_fall.cancel()
 
 
-    async def push_data(self):
-        """Periodically looks for new data to upload 1/3 of window time"""
-        while True:
-
-            try:
-
-                if self.active and self.unprocessed:
-
-                    data_rows = {}
-                    data_set = {
-                        "data": data_rows,
-                        "num": len(self.unprocessed),
-                        "test": self.labels['title'],
-                    }
-                    #add items from deque
-                    while self.unprocessed:
-                        row_ts = self.unprocessed.pop()
-                        if row_ts in self.cache:
-                            row = self.cache[row_ts]
-                            if row:
-                                data_rows[row_ts] = row
-
-                    # Finally try writing the data
-                    if data_rows:
-                        log.info(f"writing to S3")
-                        await self.write_s3(data_set)
-                    else:
-                        log.info(f"no data, skpping s3 write")
-                    # Finally Wait Some Time
-                    await asyncio.sleep(self.window / 3.0)
-
-                elif self.active:
-                    log.info(f"no data")
-                    await asyncio.sleep(self.window / 3.0)
-                else:
-                    log.info(f"not active")
-                    await asyncio.sleep(self.window / 3.0)
-
-            except Exception as e:
-                log.error(str(e), exc_info=1)
-
-
-    async def write_s3(self,data: dict,title=None):
-        """writes the dictionary to the bucket
-        :param data: a dictionary to write as json
-        :param : default='data', use to log actions ect
-        """
-        if ON_RASPI:
-            up_time = datetime.datetime.now(tz=datetime.timezone.utc)
-            data["upload_time"] = str(up_time)
-            date = up_time.date()
-            time = f"{up_time.hour}-{up_time.minute}-{up_time.second}"
-            if title is not None and title:
-                key = f"{folder}/{self.labels['title']}/{date}/{title}_{time}.json"
-            else:
-                #data
-                key = f"{folder}/{self.labels['title']}/{date}/data_{time}.json"
-
-            session = get_session()
-            async with session.create_client('s3',region_name='us-east-1',config='wavetank') as client:
-
-                resp = await client.put_object(
-                    Bucket=bucket, Key=key, Body=json.dumps(data)
-                )
-                log.info(f"success writing {key}")
-                log.debug(f"got s3 resp: {resp}")
-        else:
-            log.info("mock writing s3...")
-
-
     async def setup_encoder(self):
-        await self.pi.set_mode(self.pin_enc_A, asyncpio.INPUT)
-        await self.pi.set_mode(self.pin_enc_B, asyncpio.INPUT)
+        for i,(apin,bpin) in enumerate(self.encoder_pins):
+            await self.pi.set_mode(apin, asyncpio.INPUT)
+            await self.pi.set_mode(bpin, asyncpio.INPUT)
 
-        await self.pi.set_pull_up_down(self.pin_enc_A, asyncpio.PUD_UP)
-        await self.pi.set_pull_up_down(self.pin_enc_B, asyncpio.PUD_UP)
+            await self.pi.set_pull_up_down(apin, asyncpio.PUD_UP)
+            await self.pi.set_pull_up_down(bpin, asyncpio.PUD_UP)
 
-        ee = asyncpio.EITHER_EDGE
-        self.cbA = await self.pi.callback(self.pin_enc_A,ee , self._pulse)
-        self.cbB = await self.pi.callback(self.pin_enc_B,ee , self._pulse)
+            ee = asyncpio.EITHER_EDGE
 
-    def _pulse(self, gpio, level, tick):
+            whenpulse = self._make_pulse_func(apin,bpin,i)
+            self.cbA = await self.pi.callback(apin, ee , whenpulse)
+            self.cbB = await self.pi.callback(bpin, ee , whenpulse)
+
+    def _make_pulse_func(self,apin,bpin,inx):
+        """function to scope lambda"""
+        inx = f'enc_{inx}_last_pin'
+        f = lambda *args: self._pulse(*args,apin=apin,bpin=bpin,enc_inx=inx)
+        return f
+        
+    def _pulse(self, gpio, level, tick,apin,bpin,enc_inx):
 
         self.last[gpio] = level
 
-        if gpio != self.last[gpio]: # debounce
-            self.last['enc_last_pin'] = gpio
-            if gpio == self.pin_enc_A and level == 1:
-                if self.last[self.pin_enc_B] == 1:
+        if gpio != self.last[enc_inx]: # debounce
+            self.last[enc_inx] = gpio
+            if gpio == apin and level == 1:
+                if self.last[apin] == 1:
                     self.callback(1) #forward step
-            elif gpio == self.pin_enc_B and level == 1:
-                if self.last[self.pin_enc_A] == 1:
+                    
+            elif gpio == bpin and level == 1:
+                if self.last[bpin] == 1:
                     self.callback(-1) #reverse step
-
-
 
     #SONAR:
     async def setup_echo_sensors(self):
         """setup GPIO for reading from a list of sensor pins"""
-# 
-#         #TODO: structure for these items. put in last dict per gpio
-#         self._rising_time = None
-#         self._falling_time = None
-#         self._delta_tick = None
-
+        
         self.speed_of_sound = 343.0 #TODO: add temperature correction
         self.sound_conv = self.speed_of_sound / 2000000 #2x
 
@@ -282,8 +223,105 @@ class hardware_control:
         get the current reading
         round trip cms = round trip time / 1000000.0 * 34030
         """
-        return self.last[gpio]['dt'] * self.sound_conv
+        dt = self.last[gpio]['dt']
+        if dt is not None:
+            return  dt * self.sound_conv
+        return 0
+    
+    async def print_last(self):
+        while True:
+            
+    
+if __name__ == '__main__':
 
+    encoder_pins = [(9,10)]
+    echo_pins = [18]
+
+    hw = hardware_control(encoder_pins,echo_pins)
+    hw.setup()
+
+    loop = asyncio.get_event_loop()
+    loop.
+
+    #cal = hw.run_calibration()
+    #loop.run_until_complete(cal)
+    
+    if hw.status_ok is not True:
+        raise Exception(f'initial calibration didnt work!!!')
+
+    # sensors = hw.start_data_acquisition()
+    # controls = hw.start_controls()    
+    
+
+#     #Data Recording
+#     async def push_data(self):
+#         """Periodically looks for new data to upload 1/3 of window time"""
+#         while True:
+# 
+#             try:
+# 
+#                 if self.active and self.unprocessed:
+# 
+#                     data_rows = {}
+#                     data_set = {
+#                         "data": data_rows,
+#                         "num": len(self.unprocessed),
+#                         "test": self.labels['title'],
+#                     }
+#                     #add items from deque
+#                     while self.unprocessed:
+#                         row_ts = self.unprocessed.pop()
+#                         if row_ts in self.cache:
+#                             row = self.cache[row_ts]
+#                             if row:
+#                                 data_rows[row_ts] = row
+# 
+#                     # Finally try writing the data
+#                     if data_rows:
+#                         log.info(f"writing to S3")
+#                         await self.write_s3(data_set)
+#                     else:
+#                         log.info(f"no data, skpping s3 write")
+#                     # Finally Wait Some Time
+#                     await asyncio.sleep(self.window / 3.0)
+# 
+#                 elif self.active:
+#                     log.info(f"no data")
+#                     await asyncio.sleep(self.window / 3.0)
+#                 else:
+#                     log.info(f"not active")
+#                     await asyncio.sleep(self.window / 3.0)
+# 
+#             except Exception as e:
+#                 log.error(str(e), exc_info=1)
+# 
+# 
+#     async def write_s3(self,data: dict,title=None):
+#         """writes the dictionary to the bucket
+#         :param data: a dictionary to write as json
+#         :param : default='data', use to log actions ect
+#         """
+#         if ON_RASPI:
+#             up_time = datetime.datetime.now(tz=datetime.timezone.utc)
+#             data["upload_time"] = str(up_time)
+#             date = up_time.date()
+#             time = f"{up_time.hour}-{up_time.minute}-{up_time.second}"
+#             if title is not None and title:
+#                 key = f"{folder}/{self.labels['title']}/{date}/{title}_{time}.json"
+#             else:
+#                 #data
+#                 key = f"{folder}/{self.labels['title']}/{date}/data_{time}.json"
+# 
+#             session = get_session()
+#             async with session.create_client('s3',region_name='us-east-1',config='wavetank') as client:
+# 
+#                 resp = await client.put_object(
+#                     Bucket=bucket, Key=key, Body=json.dumps(data)
+#                 )
+#                 log.info(f"success writing {key}")
+#                 log.debug(f"got s3 resp: {resp}")
+#         else:
+#             log.info("mock writing s3...")
 
 #IMU MPU9250
 # from imusensor.MPU9250 import MPU9250
@@ -327,17 +365,5 @@ class hardware_control:
 
 #test
 
-if __name__ == '__main__':
 
-    loop = asyncio.get_event_loop()
-
-    hw = hardware_control()#TODO: pins =)
-    cal = hw.run_calibration()
-    loop.run_until_complete(cal)
-    
-    if hw.status_ok is not True:
-        raise Exception(f'initial calibration didnt work!!!')
-
-    sensors = hw.start_data_acquisition()
-    controls = hw.start_controls()
 
