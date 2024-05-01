ff6e074c58ec0da0b434c69055e32dbe72551cb9
SoundsSerious <keavin7@ottermatics.com>
2024-05-01 00:27:46 -0400

    hmm

--- waveware/control.py
+++ waveware/control.py
@@ -265,7 +265,7 @@ class stepper_control:
 #             dvds = (vnow-vlast)/(dir)
 #             coef_2 = (coef_2 + dvds)/2
 #             coef_10 = (coef_10*0.9 + dvds*0.1)
-#             coef_100 = (coef_10*0.99 + dvds*0.01)                
+#             coef_100 = (coef_100*0.99 + dvds*0.01)                
 #             DIR = 'FWD' if dir > 0 else 'REV'
 #             print(f'{DIR}|{fv} {dv}|'+' '.join([f'|{v:10.7f}' for v in (dvds,coef_2,coef_10,coef_100)]))
 #             step_count += 1        
