d1e965a53f7f985eb20219597c69490210956098
SoundsSerious <keavin7@ottermatics.com>
2024-05-05 22:09:06 -0400

    babysteps

--- waveware/control.py
+++ waveware/control.py
@@ -56,11 +56,11 @@ default_speed_mode = os.environ.get('WAVE_SPEED_DRIVE_MODE','pwm').strip().lower
 assert default_speed_mode in speed_modes
 
 #vmove=[0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.1]
-vmove=vmove_default=[0.00001,0.001,0.01,0.04]
+vmove=vmove_default=[0.001,0.01,0.04]
 
 class regular_wave:
 
-    def __init__(self,Hs=0.2,Ts=10) -> None:
+    def __init__(self,Hs=0.02,Ts=10) -> None:
         self.hs = Hs
         self.ts = Ts
         self.update()
